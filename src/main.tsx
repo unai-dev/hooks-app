306bc2584e3dff4a5194e017c6f94ec8919a1579
Unai Villar <unaidevcoding@gmail.com>
2025-12-04 20:19:10 +0100

    initialize useReducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { TasksApp } from "./useReducer/TaskApp";
 // import { HooksApp } from "./HooksApp";
 // import { TrafficLight } from "./useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./useEffect/TrafficLightWithEffect";
 // import { TrafficLightWithHook } from "./useEffect/TrafficLightWithHook";
 // import { PokemonPage } from "./examples/PokemonPage";
-import { FocusScreen } from "./useRef/focusScreen";
+// import { FocusScreen } from "./useRef/focusScreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>
 );
